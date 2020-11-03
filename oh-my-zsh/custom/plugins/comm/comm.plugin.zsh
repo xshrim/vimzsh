127d19d8dbc4d905cea3e680d8db7296a722c524
xshrim <xshrim@gmail.com>
2020-11-03 10:07:34 +0800

    update

--- oh-my-zsh/custom/plugins/comm/comm.plugin.zsh
+++ oh-my-zsh/custom/plugins/comm/comm.plugin.zsh
@@ -658,6 +658,7 @@ alias ss='ss -asntup'
 alias open='xdg-open'
 alias grep='grep -P --color=auto'
 alias glog='git log --oneline --graph --decorate'
+alias glg='git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative'
 alias rs='rsync -ahr --info=progress2 --no-i-r'
 alias gm='gomacro'
 # docker alias
